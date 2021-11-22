1116209ebdaa1f89de0516c2cb5618e79fd70289
Frederik Boster <frederik@boster.de>
2021-11-22 17:17:23 +0100

    Add e2e test for health indicator rest endpoint

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import supertest from 'supertest';
+import { HealthCheckResult, HealthIndicatorResult } from '@nestjs/terminus';
 
-describe('AppController (e2e)', () => {
+describe('Smoke Test (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        AppModule
+      ]
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/ops/health (GET)', async () => {
+    const expectedHealthIndicatorResult: HealthIndicatorResult = {
+      memory_heap: {
+        status: 'up'
+      },
+      memory_rss: {
+        status: 'up'
+      }
+    };
+
+    await supertest(app.getHttpServer())
+        .get('/ops/health')
+        .expect(200)
+        .expect({
+          status: 'ok',
+          details: expectedHealthIndicatorResult,
+          info: expectedHealthIndicatorResult,
+          error: {}
+        } as HealthCheckResult);
   });
 });
